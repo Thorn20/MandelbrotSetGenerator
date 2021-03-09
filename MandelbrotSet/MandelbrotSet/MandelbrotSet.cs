8cb4ea184a9175fa1782994e65310b55f515465d
Thorn20 <60061437+Thorn20@users.noreply.github.com>
2021-03-09 19:48:52 +1100

    Add files via upload

---
+++ MandelbrotSet/MandelbrotSet/MandelbrotSet.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MandelbrotSetProgram
+{
+    class MandelbrotSet
+    {
+        Rectangle SCREEN_SPACE;
+        RectangleF MANDELBROT_SPACE = new RectangleF(-2.5f, -1.5f, 3.5f, 3.0f);
+
+        public float focusZoom;
+        public PointF focusPoint;
+        public RectangleF focusArea;
+
+        public int[,] exitValues;
+        public Bitmap pixelValues;
+
+        public MandelbrotSet(Rectangle screenSpace)
+        {
+            SCREEN_SPACE = screenSpace;
+
+            focusZoom = 1.0f;
+            focusPoint = new PointF(MANDELBROT_SPACE.X + (MANDELBROT_SPACE.Width / 2),
+                                    MANDELBROT_SPACE.Y + (MANDELBROT_SPACE.Height / 2));
+            focusArea = new RectangleF(MANDELBROT_SPACE.Location, MANDELBROT_SPACE.Size);
+
+            exitValues = new int[SCREEN_SPACE.Width, SCREEN_SPACE.Height];
+            pixelValues = new Bitmap(SCREEN_SPACE.Width, SCREEN_SPACE.Height);
+        }
+
+        public void Generate( PointF focus, float zoom = 0.0f, int maxIterationsPerPixel = 1000)
+        {
+            focusPoint = focus;
+            focusZoom += zoom;
+            SizeF focusHalfZoomed = new SizeF((focusArea.Width / 2)/focusZoom, (focusArea.Height / 2) / focusZoom);
+            focusArea = new RectangleF(focusPoint - focusHalfZoomed, focusHalfZoomed + focusHalfZoomed);
+
+            int[] iterationTallys = new int[maxIterationsPerPixel + 1];
+
+            Parallel.For(0, exitValues.GetLength(0), px =>
+            {
+                Parallel.For(0, exitValues.GetLength(1), py =>
+                {
+                    double x0 = focusArea.X + ((focusArea.Width / SCREEN_SPACE.Width) * px);
+                    double y0 = focusArea.Y + ((focusArea.Height / SCREEN_SPACE.Height) * py);
+
+                    double x, y, x2, y2;
+                    x = y = x2 = y2 = 0.0;
+
+                    int iteration = 0;
+
+                    while (x2 + y2 <= 4 && iteration < maxIterationsPerPixel)
+                    {
+                        y = (x + x) * y + y0;
+                        x = x2 - y2 + x0;
+                        x2 = x * x;
+                        y2 = y * y;
+                        iteration++;
+                    }
+
+                    exitValues[px, py] = iteration;
+
+                    if (iteration < maxIterationsPerPixel)
+                        iterationTallys[iteration]++;
+                });
+            });
+
+            int total = exitValues.Length;
+            double hue;
+            int R, G, B;
+            R = G = B = 0;
+
+            for (int px = 0; px < exitValues.GetLength(0); px++)
+                for (int py = 0; py < exitValues.GetLength(1); py++)
+                {
+                    if (exitValues[px, py] < maxIterationsPerPixel)
+                    {
+                        hue = 0.0;
+                        for (int i = 0; i <= exitValues[px, py]; i++)
+                            hue += (double)iterationTallys[i] / total;
+
+                        R = (int)(255 * (hue * 0.33));
+                        G = (int)(255 * (hue * 0.66));
+                        B = (int)(255 * (hue * 0.99));
+
+                        pixelValues.SetPixel(px, py, Color.FromArgb(R, G, B));
+                        //pixelValues.SetPixel(px, py, Color.FromArgb((int)(Int32.MaxValue * hue)));
+                    }
+                    else pixelValues.SetPixel(px, py, Color.Black);
+                }
+        }
+    }
+}
---
+++ MandelbrotSet/MandelbrotSet/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MandelbrotSetProgram
+{
+    class Program : Form
+    {
+        const int RENDER_WIDTH = 1120;
+        const int RENDER_HEIGHT = 960;
+        Rectangle RENDER_WINDOW = new Rectangle(0, 0, RENDER_WIDTH, RENDER_HEIGHT);
+
+        const int ITERATIONS_PER_PIXEL = 1000;
+
+        BufferedGraphicsContext gfxContext;
+        BufferedGraphics gfxBuffer;
+        Graphics gfx;
+
+        MandelbrotSet mandelbrotSet;
+
+        public Program() : base()
+        {
+            this.Text = "Mandelbrot Set";
+            this.Size = new Size(RENDER_WIDTH, RENDER_HEIGHT);
+            this.CenterToScreen();
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            this.MouseDoubleClick += new MouseEventHandler(WindowDoubleClick);
+
+            mandelbrotSet = new MandelbrotSet(RENDER_WINDOW);
+            mandelbrotSet.Generate( mandelbrotSet.focusPoint);
+
+            InitializeGraphics();           
+            RenderWindow();           
+        }
+
+        void InitializeGraphics()
+        {
+            gfxContext = BufferedGraphicsManager.Current;
+            gfxContext.MaximumBuffer = this.Size;
+            if (gfxBuffer != null)
+            {
+                gfxBuffer.Dispose();
+                gfxBuffer = null;
+            }            
+            gfxBuffer = gfxContext.Allocate(this.CreateGraphics(), RENDER_WINDOW);            
+        }
+
+        void RenderWindow()
+        {
+            gfx = gfxBuffer.Graphics;
+            gfx.SmoothingMode = SmoothingMode.AntiAlias;
+            gfx.FillRectangle(new SolidBrush(Color.Black), RENDER_WINDOW);
+            gfx.DrawImage(mandelbrotSet.pixelValues, 0, 0);
+            gfxBuffer.Render(Graphics.FromHwnd(Handle));
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            gfxBuffer.Render(e.Graphics);
+        }
+
+        void WindowDoubleClick( object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                PointF focus = new PointF(mandelbrotSet.focusArea.X + (mandelbrotSet.focusArea.Width * ((float)e.X / RENDER_WINDOW.Width)),
+                                          mandelbrotSet.focusArea.Y + (mandelbrotSet.focusArea.Height * ((float)e.Y / RENDER_WINDOW.Height)));
+
+                mandelbrotSet.Generate(focus, 1.0f / mandelbrotSet.focusZoom);
+                RenderWindow();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Application.Run(new Program());
+        }
+    }
+}
